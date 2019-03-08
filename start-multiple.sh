14b8010646223ba393263c21a8ee3a54e1a1261a
balchua@yahoo.com <balchua@yahoo.com>
2019-03-08 22:41:48 +0800

    add script to start multiple pods

---
+++ start-multiple.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+n=1
+
+# continue until $n equals 5
+while [ $n -le 10 ]
+do
+	echo "Welcome $n times."
+	./pod-runner --application app1-1gb-mem -k $KUBECONFIG -n default -c /usr/src/app/main-good.sh &
+        n=$(( n+1 ))	 # increments $n
+done
+
+echo "complete."
+
+exit 0
